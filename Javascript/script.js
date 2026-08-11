350b6f3a5a742327c30a627ea5de1512d2c83ecc
GabiSosigan <gabi.sosigan@edu.unifil.br>
2026-08-11 20:15:34 -0300

    Introdução ao Javascript

---
+++ Javascript/script.js
@@ -0,0 +1,48 @@
+let nome1 = "Ana";
+let nome2 = "Beatriz";
+let nomeCompleto = nome1 + " " + nome2;
+console.log(nomeCompleto); // Concatenação de strings
+
+let oi = "Olá, ";
+oi += "Thiago!";
+console.log(oi); // Concatenação usando o operador +=
+
+let str1 = "Hello";
+let str2 = "World!";
+let resultado = str1.concat(" ", str2);
+console.log(resultado); // Concatenação usando o método concat()
+
+// Código do bot de saudação do FreeCodeCamp
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
+let bot;
+bot = "teacherBot";
+
+let botLocation = "the universe";
+
+console.log("Allow me to introduce myself.");
+
+const botIntroduction = "My name is " + bot + ".";
+console.log(botIntroduction);
+
+const botLocationSentence = "I live in " + botLocation + ".";
+console.log(botLocationSentence);
+
+bot = "professorBot";
+
+const nicknameIntroduction = "My nickname is " + bot + ".";
+console.log(nicknameIntroduction);
+
+bot = "awesomeTeacherBot";
+
+const newNicknameGreeting = "I love my nickname but I wish people would call me " + bot + ".";
+console.log(newNicknameGreeting);
+
+const favoriteSubject = "Computer Science";
+
+const favoriteSubjectSentence = "My favorite subject is " + favoriteSubject + ".";
+console.log(favoriteSubjectSentence);
+
+console.log("Well, it was nice to talk to you. Have a nice day!")
+
+
